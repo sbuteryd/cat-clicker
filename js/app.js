2c739f7cd79a2b87d632b3edb1e7bd7f7475ea2c
xie <283994917@qq.com>
2018-12-26 11:19:58 +0800

    模型第二次优化

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+
+//小猫数据模型
 const catsModel = {
     now: null,
     cats: [
@@ -29,9 +31,8 @@ const catsModel = {
     ]
 };
 
-
 const catControl = {
-
+    //cat-clicker初始化
     init() {
         catsModel.now = catsModel.cats[0];
         catView.init();
@@ -41,60 +42,69 @@ const catControl = {
     getCats() {
         return catsModel.cats;
     },
+    //获取当前点击猫
     getNowCat() {
         return catsModel.now;
     },
+    //设置当前点击猫
     setNowCat(cat) {
         catsModel.now = cat;
     },
+    //点击图片
     clickPicture() {
         catsModel.now.clickCount ++;
+        catView.render();
+    },
+    //图片计数器
+    catListClick(cat) {
+        catControl.setNowCat(cat);
         catView.render();
     }
 };
 
+// 小猫列表视图
 const catListView = {
+    //列表视图初始化
     init() {
         this.catList = document.querySelector(".cat-list");
         this.render();
     },
+    //根据小猫数量初始化列表
     render() {
-        let cats = catControl.getCats();
-        const fragment = document.createDocumentFragment();
-        for (let x = 0; x < cats.length;  x++) {
-            let cat = cats[x],
-                li = document.createElement('li');
-            li.className = `${cat.name}`;
-            li.innerText = cat.name;
-            li.addEventListener('click', function () {
-                catControl.setNowCat(cat);
-                catView.render();
-            });
+        const cats = catControl.getCats(),
+              fragment = document.createDocumentFragment();
+        //循环数组
+        cats.forEach((cat) => {
+            const li = document.createElement('li');
+            li.innerText = `${cat.name}`;
+            li.onclick = () => catControl.catListClick(cat);
             fragment.appendChild(li);
-        }
+        });
         this.catList.appendChild(fragment);
     }
 };
 
+// 小猫图片，点击次数视图
 const catView = {
+    //初始化
     init() {
         this.picture = document.querySelector(".picture");
         this.click = document.querySelector(".click-count");
         this.name = document.querySelector(".name");
         this.render();
-        this.picture.addEventListener("click", catControl.clickPicture);
-
+        //点击图片事件
+        this.picture.onclick = () => catControl.clickPicture();
     },
+    //初始化方法
     render() {
         let nowCat = catControl.getNowCat();
         this.picture.src = nowCat.imgSrc;
         this.click.textContent = nowCat.clickCount;
         this.name.textContent =  nowCat.name;
-
     }
 };
 
-
+//渲染页面
 catControl.init();
 
 
