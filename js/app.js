3345d4384529ceeccf876287a82829d777247ecf
xie <283994917@qq.com>
2018-12-26 10:29:25 +0800

    优化代码-重构

--- js/app.js
+++ js/app.js
@@ -30,74 +30,72 @@ const catsModel = {
 };
 
 
-const catController = {
+const catControl = {
 
     init() {
         catsModel.now = catsModel.cats[0];
-        catListView.init();
         catView.init();
-
+        catListView.init();
     },
+    //获取所有猫
     getCats() {
         return catsModel.cats;
     },
-
-
-};
-
-
-
-
-const catListView = {
-    init() {
-        this.catName = document.querySelector(".cat-name");
-        console.log(this.screenPicture);
-        this.render();
+    getNowCat() {
+        return catsModel.now;
     },
-
-    render() {
-        let cats = catController.getCats();
-        console.log(cats);
-        for (let x = 0; x < cats.length; x++) {
-            let li = document.createElement('li');
-            li.innerText = cats[x].name;
-            li.className = `cat-${cats[x].name}`;
-            li.addEventListener("click", function () {
-                catsModel.now = cats[x];
-                let img = document.querySelector(".picture");
-                let click = document.querySelector(".click-count");
-                let name = document.querySelector(".name");
-                img.src = cats[x].imgSrc;
-                click.textContent = cats[x].clickCount;
-                name.textContent = catsModel.now.name;
-            });
-
-            this.catName.appendChild(li);
-        }
+    setNowCat(cat) {
+        catsModel.now = cat;
+    },
+    clickPicture() {
+        catsModel.now.clickCount ++;
+        catView.render();
     }
 };
 
-
+const catListView = {
+    init() {
+        this.catList = document.querySelector(".cat-list");
+        this.render();
+    },
+    render() {
+        let cats = catControl.getCats();
+        const fragment = document.createDocumentFragment();
+        for (let x = 0; x < cats.length;  x++) {
+            let cat = cats[x],
+                li = document.createElement('li');
+            li.className = `${cat.name}`;
+            li.innerText = cat.name;
+            li.addEventListener('click', function () {
+                catControl.setNowCat(cat);
+                catView.render();
+            });
+            fragment.appendChild(li);
+        }
+        this.catList.appendChild(fragment);
+    }
+};
 
 const catView = {
     init() {
         this.picture = document.querySelector(".picture");
+        this.click = document.querySelector(".click-count");
         this.name = document.querySelector(".name");
-
-        this.name.textContent = catsModel.now.name;
-        this.picture.src = catsModel.now.imgSrc;
         this.render();
+        this.picture.addEventListener("click", catControl.clickPicture);
+
     },
     render() {
-        this.picture.addEventListener("click", function () {
-            catsModel.now.clickCount +=1;
-            let click = document.querySelector(".click-count");
-            click.textContent = catsModel.now.clickCount;
+        let nowCat = catControl.getNowCat();
+        this.picture.src = nowCat.imgSrc;
+        this.click.textContent = nowCat.clickCount;
+        this.name.textContent =  nowCat.name;
 
-        })
     }
 };
 
 
+catControl.init();
 
-catController.init();
+
+
